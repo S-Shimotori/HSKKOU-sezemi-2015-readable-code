a0c090f1d1dd5fa87d6a67d07174a2328799115e
比佐 幸基 <hskk1120551@hskk-develop.local>
2015-06-06 10:00:41 +0900

    first Commit

---
+++ RecipeManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RecipeManager
+//
+//  Created by 比佐 幸基 on 2015/06/06.
+//  Copyright (c) 2015年 比佐 幸基. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
